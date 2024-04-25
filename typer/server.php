4399a0562e519f0d51c086808a15f71f44497bf1
Taavi <144431054+tv911473@users.noreply.github.com>
2024-04-25 13:55:24 +0300

    kodutoo-2
    typer

--- typer/server.php
+++ typer/server.php
@@ -1,14 +1,26 @@
 <?php
-if(isset($_POST["save"]) && !empty($_POST["save"])){
+if (isset($_POST["save"]) && !empty($_POST["save"])) {
     saveToFile($_POST["save"]);
 }
 
-function saveToFile($stringToSave){
+function saveToFile($resultsArray) {
+
+
+    $existingData = file_get_contents("database.txt");
+    $existingResults = [];
+    if ($existingData) {
+        $decodedData = json_decode($existingData);
+        if ($decodedData->content) {
+            $existingResults = $decodedData->content;
+        }   
+    }
+
+    $existingResults[] = $resultsArray; 
+
     $object = new StdClass();
     $object->last_modified = time();
-    $object->content = $stringToSave;
+    $object->content = $existingResults;
     $jsonString = json_encode($object);
     file_put_contents("database.txt", $jsonString);
 }
-
 ?>
--- typer/typer.js
+++ typer/typer.js
@@ -1,5 +1,3 @@
-//let playerName = prompt('Palun sisesta oma nimi')
-
 class Typer{
     constructor(){
         this.name = "Anonüümne"; //kasutaja nimi, mida ta peab mängu alguses sisestam
@@ -28,7 +26,6 @@ class Typer{
     }
 
     getWords(data){
-        //console.log(data);
         const dataFromFile = data.split('\n');
         this.separateWordsByLength(dataFromFile);
     }
@@ -44,7 +41,8 @@ class Typer{
             this.words[wordLength].push(data[i]);
         }
 
-        //console.log(this.words);
+        console.log(this.words)
+
         $('#submitName').click(
             ()=>{
                 this.name = $('#nameValue').val();
@@ -58,6 +56,7 @@ class Typer{
                     this.startTyper();
                     this.startOnce();
                 }
+
             }
         );
     }
@@ -65,7 +64,6 @@ class Typer{
     startOnce(){
         $(document).on("keypress", (event)=>this.shortenWord(event.key));
         $('#restart').on('click', ()=>this.startTyper());
-        this.showResults();
     }
 
     startTyper(){
@@ -82,12 +80,11 @@ class Typer{
     }
 
     shortenWord(keypressed){
-        //console.log(keypressed);
 
         if(this.word.charAt(0) != keypressed){
-            document.getElementById('container').style.backgroundColor = "lightpink";
+            document.getElementById('container').style.backgroundColor = "red";
             setTimeout(function(){
-                document.getElementById('container').style.backgroundColor = "beige";
+                document.getElementById('container').style.backgroundColor = "#f5f5f5";
             }, 100);
         }
 
@@ -107,14 +104,11 @@ class Typer{
         }
     }
 
-
-
     generateWords(){
         for(let i = 0; i < this.wordsInGame; i++){
             const wordLength = this.startingWordLength + i;
             const randomWord = Math.round(Math.random() * 
             this.words[wordLength].length);
-            //console.log(wordLength, randomWord);
 
             this.typeWords[i] = this.words[wordLength][randomWord];
         }
@@ -136,18 +130,30 @@ class Typer{
         this.chars = 0;
         for(let i = 0; i < this.wordsInGame; i++){
             this.chars = this.chars + this.startingWordLength + i;
+            console.log(this.chars);
         }
+        
+        let wordsPerMinute = ((this.chars/((this.endTime-this.startTime)/1000)) * 60).toFixed(0);
 
         let result = {
             name: this.name,
             time: ((this.endTime-this.startTime)/1000).toFixed(2),
             words: this.wordsInGame,
-            chars: this.chars
+            chars: this.chars,
+            wordsPerMin: wordsPerMinute
+        }
+
+        if (wordsPerMinute >= 60) {
+            result.image = "pics/gold.png"; 
+        } else if (wordsPerMinute >= 50) {
+            result.image = "pics/silver.png";
+        } else {
+            result.image = "pics/bronze.png";
         }
 
         this.results.push(result);
 
-        this.results.sort((a, b) => parseFloat(a.time) - parseFloat(b.time));
+        this.results.sort((a, b) => parseFloat(b.wordsPerMin) - parseFloat(a.wordsPerMin));
 
         this.showResults();
 
@@ -158,13 +164,60 @@ class Typer{
         );
     }
 
-    showResults(){
-        $('#results').html("");
-        for(let i = 0; i < this.results.length; i++){
-            if(i === 10){break;}
-            $("#results").append((i+1) + "." + this.results[i].name + " " + this.results[i].time + " " + this.results[i].words + " " + this.results[i].chars + "<br>");
+    showResults() {
+        const resultsContainer = $('#results');
+        let table = resultsContainer.find('table');
+    
+        if (table.length === 0) {
+            table = $('<table></table>');
+    
+            table.append('<tr><th>Nimi</th><th>Aeg</th><th>Sõnad</th><th>Tähti</th><th>Sõna/min</th><th>Medal</th></tr>');
+    
+            resultsContainer.append(table);
+        } else {
+            table.find('tbody').empty();
+        }
+
+        this.results.sort((a, b) => b.wordsPerMin - a.wordsPerMin);
+
+        for (let i = 0; i < Math.min(10, this.results.length); i++) {
+            const result = this.results[i];
+    
+            const row = $('<tr></tr>');
+
+            row.append(`<td>${result.name}</td>`);
+            row.append(`<td>${result.time}</td>`);
+            row.append(`<td>${result.words}</td>`);
+            row.append(`<td>${result.chars}</td>`);
+            row.append(`<td>${result.wordsPerMin}</td>`);
+
+            const imageCell = $('<td></td>');
+            const image = $(`<img src="${result.image}" alt="${result.wordsPerMin} medal">`);
+            imageCell.append(image);
+            row.append(imageCell);  
+    
+            table.append(row);
         }
     }
 }
+
+$(document).ready(function() {
+    $('#fontSelector').on('change', function() {
+        let selectedFont = $(this).val();
+        $('body').css('font-family', selectedFont);
+    });
+});
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
 
 let typer = new Typer();
