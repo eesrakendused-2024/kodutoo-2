33cc32fe4dafb2659032bc743fbf45af0cae4f9d
ABerkman04 <146323612+ABerkman04@users.noreply.github.com>
2024-05-05 22:23:53 +0300

    See võttis jube kaua aega, aga tähtsad asjad peaksid olemas olema

--- server.php
+++ server.php
@@ -6,7 +6,7 @@ if(isset($_POST["save"]) && !empty($_POST["save"])){
 function saveToFile($stringToSave){
     $object = new StdClass();
     $object->last_modified = time();
-    $object->content = $stringToSave;
+    $object-> content = $stringToSave;
     $jsonString = json_encode($object);
     file_put_contents("database.txt", $jsonString);
 }
